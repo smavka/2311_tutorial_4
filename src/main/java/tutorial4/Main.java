3d2e4d00040cbac6557d3ef31e2e6d57ff57b340
smavka <smavka@my.yorku.ca>
2026-03-22 22:58:53 -0400

    tried out a fail test and modified build.gradle

--- src/main/java/tutorial4/Main.java
+++ src/main/java/tutorial4/Main.java
@@ -3,7 +3,15 @@ package tutorial4;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World.");
+		int a = 10;
+		int b = 20;
+		int sum = add(a, b);
+		System.out.println("The sum is: " + sum);
+		}
+	
+	public static int add(int a, int b) {
+		int s =  a + b;
+		return s;
 	}
 
 }
